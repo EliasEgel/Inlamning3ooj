1d37356afc748e4170596a985e43cd0a1b31bc81
Jani <Jani.hakala@yh.nackademin.se>
2022-10-28 14:59:53 +0200

    added buttonnumbers

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -2,27 +2,27 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameFrame extends JFrame{
-
+    Functions f = new Functions();
     JPanel mainPanel = new JPanel(new BorderLayout());
     JPanel l1 = new JPanel(new GridLayout(4,4));
     JLabel northlabel = new JLabel("", SwingConstants.CENTER);
     JPanel southPanel = new JPanel();
-    JButton b1 = new JButton();
-    JButton b2 = new JButton();
-    JButton b3 = new JButton();
-    JButton b4 = new JButton();
-    JButton b5 = new JButton();
-    JButton b6 = new JButton();
-    JButton b7 = new JButton();
-    JButton b8 = new JButton();
-    JButton b9 = new JButton();
-    JButton b10 = new JButton();
-    JButton b11 = new JButton();
-    JButton b12 = new JButton();
-    JButton b13 = new JButton();
-    JButton b14 = new JButton();
-    JButton b15 = new JButton();
-    JButton empty = new JButton();
+    JButton b1 = new JButton(f.getList().get(0));
+    JButton b2 = new JButton(f.getList().get(1));
+    JButton b3 = new JButton(f.getList().get(2));
+    JButton b4 = new JButton(f.getList().get(3));
+    JButton b5 = new JButton(f.getList().get(4));
+    JButton b6 = new JButton(f.getList().get(5));
+    JButton b7 = new JButton(f.getList().get(6));
+    JButton b8 = new JButton(f.getList().get(7));
+    JButton b9 = new JButton(f.getList().get(8));
+    JButton b10 = new JButton(f.getList().get(9));
+    JButton b11 = new JButton(f.getList().get(10));
+    JButton b12 = new JButton(f.getList().get(11));
+    JButton b13 = new JButton(f.getList().get(12));
+    JButton b14 = new JButton(f.getList().get(13));
+    JButton b15 = new JButton(f.getList().get(14));
+    JButton empty = new JButton(f.getList().get(15));
     JButton newGame = new JButton("New Game");
     GameFrame(){
 
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@
 import java.util.*;
 
 public class Main {
-    public List<String>buttonValues = Arrays.asList("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"," ");
 
     Main(){
-        Collections.shuffle(buttonValues);
         GameFrame gameFrame = new GameFrame();
     }
     public static void main(String[] args) {
