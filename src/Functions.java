59aecf01f640ba33b06974f570fe27fe77500627
EliasEgel <115075790+EliasEgel@users.noreply.github.com>
2022-10-28 14:24:09 +0200

    function/main

--- src/Functions.java
+++ src/Functions.java
@@ -1,5 +1,16 @@
+import java.util.List;
+
 public class Functions {
 
     //kontrollfunktioner
+
+    public boolean checkWon(String toCheck){
+        boolean hasWon = false;
+        if(toCheck.equals("123456789101112131415 ")){
+            hasWon=true;
+        }
+        return hasWon;
+    }
+
 
 }
