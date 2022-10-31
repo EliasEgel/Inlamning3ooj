65ff495ec5374d6c5642274d2c563d6fef2db21d
EliasEgel <115075790+EliasEgel@users.noreply.github.com>
2022-10-31 10:36:37 +0100

    functions

--- src/Functions.java
+++ src/Functions.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 public class Functions {
 
-    //kontrollfunktioner
-    public List<String>buttonValues = Arrays.asList("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"," ");
+    private List<String>buttonValues = Arrays.asList("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"," ");
 
     public List<String> getList(){
-        //Collections.shuffle(buttonValues);
         return buttonValues;
     }
 
--- src/GameFrame.java
+++ src/GameFrame.java
@@ -3,7 +3,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class GameFrame extends JFrame implements ActionListener {
